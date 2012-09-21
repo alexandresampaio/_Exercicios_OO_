a6bda58135cd1f5d0bd4d5d20648e7fe900c3f4b
Alexandre Sampaio <allexonrails@gmail.com>
2012-09-21 18:51:13 -0300

    listaOO

--- quadrado.py
+++ quadrado.py
@@ -4,9 +4,22 @@
 #alterar e consultar o tamanho dos lados e obter a área.
 
 class Quadrado(object):
-    def __init__(self,lado):
-        self.lado = lado
+    def __init__(self, lado):
+        self._validar_lado(lado)
+        self._lado = lado
     
     def alterar_lado(self, novo_lado):
-        self.lado = novo_lado
-        
+        self._validar_lado(novo_lado)
+        self._lado = novo_lado
+    
+    def consultar_lado(self):
+        return self._lado
+    
+    def calcular_area(self):
+        return self._lado * self.lado
+    
+    def _validar_lado(self, lado):
+        if lado <= 0:
+            raise ValueError
+    
+    lado = property(consultar_lado, alterar_lado)
